c282f933365235bdc58b6300d1d4aeea2194a856
Buyanova <125025524+Buyanova@users.noreply.github.com>
2023-04-23 11:13:34 +0300

    /

--- BusinessLogic/Services/CorzinaService.cs
+++ BusinessLogic/Services/CorzinaService.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessLogic.Services
-{
+{ 
     public class CorzinaService : ICorzinaService
     {
         private IRepositoryWrapper _repositoryWrapper;
